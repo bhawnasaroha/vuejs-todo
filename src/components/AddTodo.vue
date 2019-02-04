8a60e536949efea61535fd7b165fd679f95c741a
Bhawna Saroha <30bhawna95@gmail.com>
2019-02-05 00:48:57 +0530

    todo app built with routing and api call

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit="addTodo">
+    <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+    <input type="submit" value="Submit" class="btn">
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data(){
+    return{
+      title: ''
+    }
+  },
+  methods: {
+    addTodo(e){
+      e.preventDefault();
+      const newTodo={
+        title: this.title,
+        status: false
+      }
+      // send up to parent
+      this.$emit('add-todo', newTodo);
+      this.title='';
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      <todo-item :todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"></todo-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  props: ["todos"],
+  components: {
+    TodoItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
